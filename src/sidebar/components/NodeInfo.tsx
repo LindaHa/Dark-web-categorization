750b64b4bed9bcc8e3365aec543a475fdfee8ac9
LindaHa <hanslik.linda@gmail.com>
2019-06-16 23:48:35 +0200

    Style selected node info to be pleasing

--- src/sidebar/components/NodeInfo.tsx
+++ src/sidebar/components/NodeInfo.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import * as Immutable from 'immutable';
 import * as PropTypes from 'prop-types';
+import * as ImmutablePropTypes from 'immutable-prop-types';
 import { IPage } from '../../models/page';
 
 export interface INodeInfoDataProps {
   readonly selectedNode: IPage;
+  readonly nodeLinks: Immutable.Set<string>;
 }
 
 export interface INodeInfoCallbackProps {
@@ -15,19 +18,44 @@ export class NodeInfo extends React.PureComponent<NodeInfoProps> {
   static displayName = 'NodeInfo';
   static propTypes: PropTypesShape<NodeInfoProps> = {
     selectedNode: PropTypes.object.isRequired,
+    nodeLinks: ImmutablePropTypes.set,
   };
 
   render() {
-    const {selectedNode} = this.props;
+    const {selectedNode, nodeLinks} = this.props;
     return (
       <div>
-        <div>
-          <div>Url</div>
-          <div>{selectedNode.url}</div>
+        <div className="sidebar__info-group">
+          <div className="sidebar__info-group-label-item">
+            Url
+          </div>
+          <div className="sidebar__info-group-value-item">
+            {selectedNode.url}
+          </div>
+
         </div>
-        <div>
-          <div>Categories</div>
-          <div>{selectedNode.categories}</div>
+        <div className="sidebar__info-group">
+          <div className="sidebar__info-group-label-item">Categories</div>
+          {selectedNode.categories.map((category: string, index: string) =>
+            <div
+              key={index}
+              className="sidebar__info-group-value-item"
+            >
+              {category}
+            </div>
+          )}
+        </div>
+
+        <div className="sidebar__info-group">
+          <div className="sidebar__info-group-label-item">Linked pages</div>
+          {nodeLinks.map((link: string) =>
+            <div
+              key={link}
+              className="sidebar__info-group-value-item"
+            >
+              {link}
+            </div>
+          )}
         </div>
       </div>
     );
--- src/sidebar/containers/NodeInfo.ts
+++ src/sidebar/containers/NodeInfo.ts
@@ -1,12 +1,26 @@
 import { connect } from 'react-redux';
+import * as Immutable from 'immutable';
 import { IState } from '../../_shared/models/IState';
 import {
   INodeInfoDataProps,
   NodeInfo as NodeInfoComponent,
 } from '../components/NodeInfo';
 
-const mapStateToProps = (state: IState): INodeInfoDataProps => ({
-  selectedNode: state.nodes.get(state.selectedNode)!,
-});
+const mapStateToProps = (state: IState): INodeInfoDataProps => {
+  const nodes = state.nodes;
+  const selectedNode = nodes.get(state.selectedNode)!;
+  // @ts-ignore
+  const nodeLinks: Immutable.Set<string> = selectedNode.links
+    .map((linkId: Uuid) => {
+      const linkedNode = nodes.get(linkId);
+      return linkedNode && linkedNode.url;
+    })
+    .filter((url: string | undefined) => !!url);
+
+  return {
+    selectedNode,
+    nodeLinks,
+  };
+};
 
 export const NodeInfo = connect(mapStateToProps)(NodeInfoComponent);
