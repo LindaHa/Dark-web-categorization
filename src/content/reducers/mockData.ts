6116c9203a49840a098d89fcbfe5b8f2e61f1eda
lindaha <hanslik.linda@gmail.com>
2019-10-22 17:14:35 +0200

    Adjust mock data a little

--- src/content/reducers/mockData.ts
+++ src/content/reducers/mockData.ts
@@ -1,62 +1,127 @@
 import {
-  IPage,
-  Page
-} from '../../models/page';
+  INode,
+  Node
+} from '../../models/node';
 import * as Immutable from 'immutable';
 
-export const mockPages = Immutable.Set<IPage>([
-  new Page({
-    id: '9ed26dd9-738a-49f1-91a6-91ed5793fe5d',
-    description: 'Sleep some',
-    url: 'xordoquy.onion',
+export const mockNodes = Immutable.Set<INode>([
+  new Node({
+    id: 'xordoquy.onion',
+    firstMembers: Immutable.List<INode>(),
     categories: Immutable.Set<string>(['guns, drugs']),
-    links: Immutable.Set<Uuid>(['xordoquy.onion/guns', 'xordoquy.onion/drugs'])
+    links: Immutable.Set<Uuid>(['xordoquy.onion/guns', 'xordoquy.onion/drugs']),
+    membersCount: 1,
   }),
-  new Page({id: 'a07f0bc2-8bdb-4a0e-85f6-a037d71affe9', description: 'Sleep more', url: 'xordoquy.onion/guns', categories: Immutable.Set<string>(['guns'])}),
-  new Page({
-    id: 'a077a831-6d9f-4d9c-ab1d-9ec3fb6abd31',
-    description: 'Sleep less',
-    url: 'xordoquy.onion/drugs',
+  new Node({
+    id: 'xordoquy.onion/guns',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['guns']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'xordoquy.onion/drugs',
+    firstMembers: Immutable.List<INode>(),
     categories: Immutable.Set<string>(['drugs']),
-    links: Immutable.Set<Uuid>(['wow.onion/drugs', 'road.onion/drugs'])
+    links: Immutable.Set<Uuid>(['wow.onion/drugs', 'road.onion/drugs']),
+    membersCount: 1,
   }),
-  new Page({
-    id: 'b4889aed-c92f-4533-a5e0-0f6b600cd276',
-    description: 'Wow some',
-    url: 'wow.onion',
+  new Node({
+    id: 'wow.onion',
+    firstMembers: Immutable.List<INode>(),
     categories: Immutable.Set<string>(['drugs, hacking, money laundry']),
-    links: Immutable.Set<Uuid>(['wow.onion/drugs', 'wow.onion/hacking', 'wow.onion/laundry'])
+    links: Immutable.Set<Uuid>(['wow.onion/drugs', 'wow.onion/hacking', 'wow.onion/laundry']),
+    membersCount: 1,
   }),
-  new Page({id: 'f5e24f18-9878-4750-82ea-8026d0472baa', description: 'Wow better', url: 'wow.onion/drugs', categories: Immutable.Set<string>(['drugs'])}),
-  new Page({
-    id: '3f35e4ff-93fb-4796-9c11-385ada0392df',
-    description: 'Wow less',
-    url: 'wow.onion/hacking',
+  new Node({
+    id: 'wow.onion/drugs',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['drugs']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'wow.onion/hacking',
+    firstMembers: Immutable.List<INode>(),
     categories: Immutable.Set<string>(['hacking']),
-    links: Immutable.Set<Uuid>(['for4.onion/hacking', 'for4.onion'])
+    links: Immutable.Set<Uuid>(['for4.onion/hacking', 'for4.onion']),
+    membersCount: 1,
   }),
-  new Page({id: 'e12daf5d-ac6b-4acb-b789-725917f314d4', description: 'Wow cleaner', url: 'wow.onion/laundry', categories: Immutable.Set<string>(['money laundry'])}),
-  new Page({id: '9ed26dd9-738a-49f1-91a6-91ed5793fe5d', description: 'Bling some', url: 'bling.onion', categories: Immutable.Set<string>(['guns']), links: Immutable.Set<Uuid>(['bling.onion/guns'])}),
-  new Page({id: 'a6df0bc2-8bdb-4a0e-85f6-a037d71affe9', description: 'Bling more', url: 'bling.onion/guns', categories: Immutable.Set<string>(['guns'])}),
-  new Page({
-    id: 'abef0bc2-8bdb-4a0e-85f6-a037d71affe9',
-    description: 'Drive some',
-    url: 'road.onion/',
+  new Node({
+    id: 'wow.onion/laundry',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['money laundry']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'bling.onion',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['guns']),
+    links: Immutable.Set<Uuid>(['bling.onion/guns']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'bling.onion/guns',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['guns']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'road.onion/',
+    firstMembers: Immutable.List<INode>(),
     categories: Immutable.Set<string>(['guns, porn, drugs']),
-    links: Immutable.Set<Uuid>(['road.onion/guns', 'road.onion/porn', 'road.onion/drugs'])
+    links: Immutable.Set<Uuid>(['road.onion/guns', 'road.onion/porn', 'road.onion/drugs']),
+    membersCount: 1,
   }),
-  new Page({id: 'a079dbc2-8bdb-4a0e-85f6-a03ets1afer9', description: 'Drive faster', url: 'road.onion/guns', categories: Immutable.Set<string>(['guns'])}),
-  new Page({id: 'a07f0as2-8sfb-4a0e-85f6-a037d71a4dd9', description: 'Drive less', url: 'road.onion/porn', categories: Immutable.Set<string>(['guns']), links: Immutable.Set<Uuid>(['xordoquy.onion'])}),
-  new Page({id: 'a077a831-6dsd-4d9c-ab1d-9ec3fb6abd31', description: 'Drive better', url: 'road.onion/drugs', categories: Immutable.Set<string>(['drugs'])}),
-  new Page({
-    id: '67589aed-c92f-df33-a5e0-0f6b600cd276',
-    description: 'Code some',
-    url: 'for4.onion',
+  new Node({
+    id: 'road.onion/guns',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['guns']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'road.onion/porn',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['guns']),
+    links: Immutable.Set<Uuid>(['xordoquy.onion']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'road.onion/drugs',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['drugs']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'for4.onion',
+    firstMembers: Immutable.List<INode>(),
     categories: Immutable.Set<string>(['drugs, hacking']),
-    links: Immutable.Set<Uuid>(['for4.onion/drugs', 'for4.onion/hacking'])
+    links: Immutable.Set<Uuid>(['for4.onion/drugs', 'for4.onion/hacking']),
+    membersCount: 1,
   }),
-  new Page({id: 'gbc24f18-df78-47sa-82ea-8026d047a344', description: 'Code better', url: 'for4.onion/drugs', categories: Immutable.Set<string>(['drugs'])}),
-  new Page({id: 'af35e4ff-93sb-4796-asf1-385ada0392df', description: 'Code faster', url: 'for4.onion/hacking', categories: Immutable.Set<string>(['hacking']), links: Immutable.Set<Uuid>(['wow.onion/hacking'])}),
-  new Page({id: 'e12daf5d-ac6b-4acb-b789-s45d43f314d4', description: 'Do some', url: 'blrn.onion', categories: Immutable.Set<string>(['services']), links: Immutable.Set<Uuid>(['blrn.onion/services'])}),
-  new Page({id: 'af340a1c-ce9a-4c58-b82c-8d5435ds4daf', description: 'Do better', url: 'blrn.onion/services', categories: Immutable.Set<string>(['services']), links: Immutable.Set<Uuid>(['for4.onion/hacking'])}),
+  new Node({
+    id: 'for4.onion/drugs',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['drugs']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'for4.onion/hacking',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['hacking']),
+    links: Immutable.Set<Uuid>(['wow.onion/hacking']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'blrn.onion',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['services']),
+    links: Immutable.Set<Uuid>(['blrn.onion/services']),
+    membersCount: 1,
+  }),
+  new Node({
+    id: 'blrn.onion/services',
+    firstMembers: Immutable.List<INode>(),
+    categories: Immutable.Set<string>(['services']),
+    links: Immutable.Set<Uuid>(['for4.onion/hacking']),
+    membersCount: 1,
+  }),
 ]);
