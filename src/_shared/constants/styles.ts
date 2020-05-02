bdb7fe2806755fc43b96142fc5d56d2597c5b09e
lindaha <hanslik.linda@gmail.com>
2020-05-03 00:53:51 +0200

    Adjust category colours

--- src/_shared/constants/styles.ts
+++ src/_shared/constants/styles.ts
@@ -1,8 +1,9 @@
 // const blue50 = '#e2e4eb';
 // const blue100 = '#b6bbce';
 // const blue200 = '#8790ac';
-const blue200 = '#8790ac';
+// const blue200 = '#8790ac';
 // const blue300 = '#5b668c';
+// const blue400 = '#3b4977';
 const brightBlue = '#2323ff';
 // const blue500 = '#172e64';
 // const blue600 = '#11285c';
@@ -24,17 +25,18 @@ const teal900 = '#00312e';
 const brown300 = '#9b554a';
 // const brown800 = '#7d2627';
 
-const green400 = '#4ca368';
+// const green400 = '#4ca368';
 // const green700 = '#5cd254';
-const green900 = '#33691e';
+// const green900 = '#33691e';
 
 // const red900 = '#d20000';
 const orange = '#de8500';
 const mustardYellow = '#a1a600';
 
-const pink = '#ff80e9';
-const hotPink = '#dc2cbe';
+const pink = '#e8acac';
 // const deepPurple500 = '#9200ff';
+const lila = '#a97ed6';
+const gray300 = '#9e9e9e';
 
 
 export const primaryNodeColor = teal600;
@@ -48,14 +50,13 @@ export const primaryFontColor = teal900;
 export const graphHighlightSector = mustardYellow;
 
 export const CategoryColours: { [category: string]: string; } = {
-  'Sexual Content': blue200,
-  'Illegal services and goods': green400,
-  'Finance and Fraud': blue800,
-  Gambling: brightBlue,
-  'Hosting and Programming and Hacking': pink,
-  'Online Marketplace': hotPink,
-  Other: green900,
-  // Porn: brown800,
+  'Sexual Content': gray300,
+  'Illegal services and goods': teal200,
+  'Finance and Fraud': teal600,
+  Gambling: blue800,
+  'Hosting and Programming and Hacking': brightBlue,
+  'Online Marketplace': lila,
+  Other: pink,
   Social: brown300,
   'Web Catalogue': orange,
 };
